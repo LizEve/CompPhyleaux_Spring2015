d518e77362a8b1f44035f3cb7bd7f2cd4923ff34
Genevieve Mount <ggmountt@gmail.com>
2015-01-29 10:41:18 -0600

    Create random_shit.py

---
+++ random_shit.py
@@ -0,0 +1,9 @@
+#DEFAULT VALUES 
+#you can do a def like this
+def myfunction(x=1,y=2):
+  do things in this function
+#this will mean that if the user just run the function as:
+myfunction()
+#it will automatically run the funtion with the default values of 1 and 2
+
+
