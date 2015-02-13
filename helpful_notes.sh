ea7c8e6a9d73ae08be0bb34efce444dc2ae15690
Genevieve Mount <gmount1@lsu.edu>
2015-02-13 16:50:40 -0600

    "more short scripts"

--- helpful_notes.sh
+++ helpful_notes.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
--ADD EXTENSION
+-add file extensions recursively 
 	for f in jmodg_*; do mv "$f" "$f.nex"; done
--REMOVE EXTENSION
+	filler line
+
+-remove file extensions recursively 
 	for f in jmodg_*.nex; do mv "$f" "${f%.*}"; done
+	filler line
+
+-move files into folders according to name
+	for f in *.nex; do base=`basename $f .nex`; cd $base; mv "../jmodg_$base" . ; cd ../ ; done
+	for f in *.nex;  do base=`basename $f .nex`; cd $base; cp "../$base.nex" .; cd ../; done
+
+-iterate through files, run program
+	for f in squamg_*; do cd $f; Garli-2.0 -b $f.garli.conf; cd ../;done
+
+
+
+
+
+
+
+
+
+=====___LINKS____================================================================================================
+
 !Helpful bash syntax info
 	-http://www.tldp.org/LDP/abs/html/parameter-substitution.html#PSOREX1
